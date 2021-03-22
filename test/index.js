b58dc4617e239cdaa7796cf2ad00011e55d80597
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2021-03-22 16:12:04 -0400

    chore: fix test

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 let tarInstall = require('..')
 let tarUrl = require('tar-url')
 let path = require('path')
-let fs = require('fsx')
+let fs = require('saxon/sync')
 
 let dep = process.argv[2] || 'lodash'
 let ver = process.argv[3]
 ;(async function() {
   let url = await tarUrl(dep, ver)
   let dest = __dirname + '/' + path.basename(url, '.tgz')
-  if (fs.isDir(dest)) fs.removeDir(dest)
+  fs.remove(dest, true)
   console.log('Installing: ' + url)
   let started = Date.now()
   await tarInstall(url, __dirname)
